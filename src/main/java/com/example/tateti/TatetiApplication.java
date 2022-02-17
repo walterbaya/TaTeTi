b6bc96478e294ac7f02c6fdd888a1e72bb3767c9
walterbaya <walterbaya@yahoo.com>
2022-02-16 23:05:22 -0300

    comenzamos el cambio de enfoque de TaTeTi a juego en línea

---
+++ src/main/java/com/example/tateti/TatetiApplication.java
@@ -0,0 +1,33 @@
+package com.example.tateti;
+
+import com.example.tateti.model.BotPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TatetiApplication implements CommandLineRunner {
+
+	@Autowired
+	Board board;
+
+	Logger log = LoggerFactory.getLogger(TatetiApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(TatetiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		BotPlayer bot = new BotPlayer();
+		//HumanPlayer hp = new HumanPlayer();
+		bot.play(1,1);
+		//hp.play(2,1);
+		//board.setPosition(0,0,true);
+		//log.info("se imprime tablero de posiciones: " + board.printPositions());
+	}
+}
---
+++ src/test/java/com/example/tateti/TatetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tateti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TatetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
